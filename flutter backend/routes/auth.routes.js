4520f878dcf70f05458366d64835e8b818e9e0dd
Harshil828 <harshillad9090@gmail.com>
2026-03-24 13:41:56 +0530

    Delete flutter backend directory

--- flutter backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signup, verifyOTP, login } = require("../controllers/auth.controller");
-
-router.post("/signup", signup);
-router.post("/verify-otp", verifyOTP);
-router.post("/login", login);
-
-module.exports = router;
