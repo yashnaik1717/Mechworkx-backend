bf4b18bbf5d45d1a7cf4ce0cc41d50749f12926b
Harshil828 <harshillad9090@gmail.com>
2026-03-24 13:42:06 +0530

    Delete web backend directory

--- web backend/README.md
+++
@@ -1,1 +0,0 @@
-# MechWorkx_backend
--- web backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { auth } = require("../middleware/authMiddleware");
-const { sendMessage, getJobMessages } = require("../controllers/messageController");
-
-router.post("/", auth, sendMessage);
-router.get("/job/:jobId", auth, getJobMessages);
-
-module.exports = router;
